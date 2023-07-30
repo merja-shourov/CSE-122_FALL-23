ff7f64c6a9aace70f4908d026cf45394bba4cd20
Merja Shourov <73456246+merja-shourov@users.noreply.github.com>
2023-07-30 15:04:22 +0600

    upload

---
+++ Lab-01/prob-1.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    cout << "Hello in my university\n\n";
+    cout << "Im a student in it faculty\n\n";
+    cout << "Welcome.\n";
+    return 0;
+}
