b901c320210efb12b10ef62079d5d96b0ff931df
Merja Shourov <73456246+merja-shourov@users.noreply.github.com>
2023-08-06 16:18:53 +0600

    Update 4A-watermelon.cpp

--- Lab-02/4A-watermelon.cpp
+++ Lab-02/4A-watermelon.cpp
@@ -3,10 +3,13 @@ using namespace std;
 
 int main(){
     int x; cin >> x;
-    if( x % 2 == 0 && x > 2 ){
-        cout << "YES\n";
-    }else{
-        cout << "NO\n";
+    if( x <= 100 ){
+        if( x % 2 == 0 && x > 2 ){
+            cout << "YES\n";
+        }else{
+            cout << "NO\n";
+        }
     }
+
     return 0;
 }
